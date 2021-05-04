7cd0bedb77d462367f71b85c1040510504a9fddb
Hyeonji-Jung <hyeojung@student.42seoul.kr>
2021-05-04 17:25:56 +0900

    0504

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,18 +6,18 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 14:11:23 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/03 15:10:06 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/04 17:25:04 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlcpy(char *dest, const char *src, size_t destsize)
+size_t		ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	while (src[i] && i + 1 < destsize)
+	while (src[i] && i + 1 < size)
 	{
 		dest[i] = src[i];
 		i++;
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 12:31:24 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/04 17:07:40 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/04 17:25:41 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@
 void	*ft_bzero(void *dest, size_t size);
 void	*ft_memset(void *dest, int c, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t size);
+void	*ft_memccpy(void *dest, const void *src, int c, sizr_t size);
+void	*ft_memchr(const void *s, int c, size_t size);
 void	*ft_memmove(void *dest, const void *src, size_t size);
 
 int		ft_isascii(int c);
@@ -31,5 +33,9 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_memcmp(const void *dest, const void *src, size_t size);
+
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
