74a80aa88418fdf1385d53f9b4508d23ff091e45
Hyeonji Jung <hyeojung@c10r6s4.42seoul.kr>
2021-05-04 16:32:53 +0900

    0504

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,21 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 15:29:53 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/03 15:56:27 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/04 16:32:46 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memmove(void *dest, const void *src, size_t n)
-{}
+void		*ft_memmove(void *dest, const void *src, size_t size)
+{
+	char	*tmp;
+	
+	tmp = (char *)malloc(size);
+	if (tmp == NULL)
+		return (NULL);
+	ft_memcpy(tmp, src, size);
+	ft_memcpy(dest, tmp, size);
+	free(tmp);
+	return (dest);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 12:31:24 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/04 12:40:53 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/04 16:07:52 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void	*ft_bzero(void *dest, size_t size);
+void	*ft_memset(void *dest, int c, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t size);
+void	*ft_memmove(void *dest, const void *src, size_t size);
+
+int		ft_isascii(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
