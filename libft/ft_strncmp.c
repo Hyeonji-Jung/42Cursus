b7afd27eb911a9a218871677c4d53781311e5e14
Hyeonji-Jung <hyeojung@student.42seoul.kr>
2021-05-11 16:14:12 +0900

    0511

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 13:40:21 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/06 17:54:30 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/11 16:09:25 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t size)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < size)
+	while (i < size)
 	{
-		if (s1[i] == s2[i])
-			return ((size_t)(s1[i] - s2[i]));
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (!s1[i] || !s2[i])
+			break ;
 		i++;
 	}
-	return ((size_t)(s1[i] - s2[i]));
+	return (0);
 }
