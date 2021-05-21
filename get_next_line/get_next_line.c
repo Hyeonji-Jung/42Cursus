172b018e878a0abd375c199e3c4bd5e8af7f716b
Hyeonji-Jung <hyeojung@student.42seoul.kr>
2021-05-21 16:43:33 +0900

    0521

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,27 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 12:59:36 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/21 12:59:48 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/21 15:29:44 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+
+
+int	get_next_line(int fd, char **line)
+{
+	int			read_fd;
+	static char	*static_buff[OPEN_MAX];
+	char		*buff;
+
+	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0 || !line)
+		return (-1);
+	if (!(buff = (char*)malloc(BUFFER_SIZE + 1)))
+		return (-1);
+	if ((read_fd = read(fd, buff, BUFFER_SIZE)) == -1)
+	{
+		free(buff);
+		return (-1);
+	}
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 12:59:12 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/05/21 12:59:20 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/05/21 13:51:10 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
