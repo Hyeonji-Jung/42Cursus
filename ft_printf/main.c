5d1d99d9dee916b910e85d50f8a4f0b9b445c6d3
Hyeonji-Jung <hyeojung@student.42seoul.kr>
2021-07-08 18:25:59 +0900

    0709

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,45 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_print_type.c                                 :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeojung <hyeojung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/16 12:37:09 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/07/08 18:22:39 by hyeojung         ###   ########.fr       */
+/*   Created: 2021/07/08 18:12:17 by hyeojung          #+#    #+#             */
+/*   Updated: 2021/07/08 18:25:40 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-int	printf_c(t_flag *flag, va_list *ap)
+int main(void)
 {
-	char	c;
+    char c = 'a';
 
-	c = va_arg(*ap, int);
-	ft_putchar(c);
-	flag->ret++;
-	return (0);
-}
-
-int	printf_s(t_flag *flag, va_list *ap)
-{
-	(void)flag;
-	(void)ap;
-	return (0);
-}
-
-int	printf_p(t_flag *flag, va_list *ap)
-{
-	(void)flag;
-	(void)ap;
-	return (0);
-}
-
-int	printf_per(t_flag *flag, va_list *ap)
-{
-	(void)flag;
-	(void)ap;
-	ft_putchar('%');
-	return (0);
+    printf("char: %5c\n", c);
+    ft_printf("char: %5c\n", c);
 }
